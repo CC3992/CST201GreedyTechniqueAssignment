621c7fb1d550c3481952afbd5c6f4feeeb0f1c4e
CC3992 <cc39923618@gmail.com>
2026-05-01 22:25:19 -0500

    Added all three greedy techniques.

--- CST201GreedyTechniqueAssignment/Program.cs
+++ CST201GreedyTechniqueAssignment/Program.cs
@@ -1,8 +1,12 @@
-﻿
-using CST201GreedyTechniqueAssignment;
+﻿using CST201GreedyTechniqueAssignment;
+//GeeksforGeeks. (2012, March 19). 0/1 Knapsack Problem. GeeksforGeeks. https://www.geeksforgeeks.org/dsa/0-1-knapsack-problem-dp-10/
+‌
 
 // Initialize variables
-int maximumWeight = 280;
+int regularMaximumWeight = 280;
+int quantityMaximumWeight = 280;
+int dynamicMaximumWeight = 280;
+
 List<Item> items = new List<Item>
 {
     new Item { weight = 20, value = 70, quantity = 1 },
@@ -12,7 +16,9 @@ List<Item> items = new List<Item>
     new Item { weight = 70, value = 120, quantity = 1 },
     new Item { weight = 90, value = 200, quantity = 2 }
 };
-int totalValue = 0;
+int regularTotalValue = 0;
+int quantityTotalValue = 0;
+int dynamicTotalValue = 0;  
 int regularGreedySteps = 0;
 int quantityGreedySteps = 0;
 int dynamicGreedySteps = 0;
@@ -26,25 +32,25 @@ int dynamicGreedySteps = 0;
     {
         regularGreedySteps++;
         Item item = items[i];
-        while (item.weight <= maximumWeight)
+        while (item.weight <= regularMaximumWeight)
         {
         regularGreedySteps++;
             // subtract weight from maximum weight
-            maximumWeight = maximumWeight - item.weight;
+            regularMaximumWeight = regularMaximumWeight - item.weight;
             //update the total value
-            totalValue = totalValue + item.value;
+            regularTotalValue = regularTotalValue + item.value;
         }
     }
 Console.WriteLine("Regular Greedy Technique");
-Console.WriteLine("Maximum Value: " + totalValue);
-Console.WriteLine("Steps: " + dynamicGreedySteps);
+Console.WriteLine("Maximum Value: " + regularTotalValue);
+Console.WriteLine("Steps: " + regularGreedySteps);
 
 
 // Dynamic greedy technique
 
-int[] bestValueAtWeight = new int[maximumWeight + 1];
+int[] bestValueAtWeight = new int[dynamicMaximumWeight + 1];
 
-for (int currentWeight = 0; currentWeight <= maximumWeight; currentWeight++)
+for (int currentWeight = 0; currentWeight <= dynamicMaximumWeight; currentWeight++)
 {
     for (int i = 0; i < items.Count; i++)
     {
@@ -66,7 +72,30 @@ for (int currentWeight = 0; currentWeight <= maximumWeight; currentWeight++)
 }
 
 Console.WriteLine("Dynamic Programming Result");
-Console.WriteLine("Maximum Value: " + bestValueAtWeight[maximumWeight]);
+Console.WriteLine("Maximum Value: " + bestValueAtWeight[dynamicMaximumWeight]);
 Console.WriteLine("Steps: " + dynamicGreedySteps);
+
+
+
+// Quantity greedy technique
+
+for (int i = 0; i < items.Count; i++)
+{
+    quantityGreedySteps++;
+    Item item = items[i];
+    while (item.weight <= quantityMaximumWeight && item.quantity > 0)
+    {
+        quantityGreedySteps++;
+        // subtract weight from maximum weight
+        quantityMaximumWeight = quantityMaximumWeight - item.weight;
+        //update the total value
+        quantityTotalValue = quantityTotalValue + item.value;
+        // decrease the quantity
+        item.quantity--;    
+    }
+}
+Console.WriteLine("Quantity Greedy Technique");
+Console.WriteLine("Maximum Value: " + quantityTotalValue);
+Console.WriteLine("Steps: " + quantityGreedySteps);
 
 
